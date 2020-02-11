645495684856256265ebf869e23bcee3fbc4c7e3
Nicolas Vega <nico_vt@protonmail.com>
2020-02-11 20:48:07 +0100

    Added Site MetaData

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,17 @@
 
 module.exports = {
     siteMetadata: {
-
+        title: 'Nvegater',
+        description: 'Portofolio, Mediary and Blog of any-thing/one I find interesting',
+        siteUrl: 'https://nvegater.com',
+        author: {
+            name: 'Nicolas Vega Terrazas',
+            url: 'https://github.com/nvegater',
+            email: 'nico_vt@protonmail.com',
+        },
+        social: {
+            github: 'https://github.com/nvegater',
+        },
     },
     plugins: [],
 }
