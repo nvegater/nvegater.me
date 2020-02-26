05d5971f7a80e848e52306be3afe6aa50fe10c17
nvegater <nico_vt@protonmail.com>
2020-02-26 15:49:39 +0100

    Added Head with Helmet

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -37,70 +37,70 @@ interface HeadProps {
   children: ReactNode;
 }
 
-const Head:FC<HeadProps> = ({
-                              title,
-                              description,
-                              lang,
-                              keywords,
-                              children}) => {
+const Head: FC<HeadProps> = ({
+                               title,
+                               description,
+                               lang,
+                               keywords,
+                               children
+                             }) => {
 
   console.log(description, keywords, children);
 
   return (
-  <StaticQuery
-    query={graphqlQuery}
-    render={(data:StaticQueryData) => {
-      const metaDescription = description || data.site.siteMetadata.description;
-      return (
-        <Helmet
-          htmlAttributes={{lang}}
-          title={title}
-          titleTemplate={`%s | ${data.site.siteMetadata.title}`}
-          meta={
-            [
-              {
-                name: `description`,
-                content: metaDescription,
-              },
-              {
-                property: `og:title`,
-                content: title,
-              },
-              {
-                property: `og:description`,
-                content: metaDescription,
-              },
-              {
-                property: `og:type`,
-                content: `website`,
-              },
-              {
-                name: `twitter:card`,
-                content: `summary`,
-              },
-              {
-                name: `twitter:creator`,
-                content: data.site.siteMetadata.author.name,
-              },
-              {
-                name: `twitter:title`,
-                content: title,
-              },
-              {
-                name: `twitter:description`,
-                content: metaDescription,
-              },
-            ].concat(
-              keywords.length > 0 ? {
-                name: 'keyboards',
-                content: keywords.join(', ')
-              } : []
-            )
-          }
-        />
-      )
-    }}
-  />
+    <StaticQuery
+      query={graphqlQuery}
+      render={
+        (data: StaticQueryData) => {
+          const metaDescription = description || data.site.siteMetadata.description;
+          return (
+            <Helmet
+              htmlAttributes={{lang}} title={title} titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+              meta={
+                [
+                  {
+                    name: `description`,
+                    content: metaDescription,
+                  },
+                  {
+                    property: `og:title`,
+                    content: title,
+                  },
+                  {
+                    property: `og:description`,
+                    content: metaDescription,
+                  },
+                  {
+                    property: `og:type`,
+                    content: `website`,
+                  },
+                  {
+                    name: `twitter:card`,
+                    content: `summary`,
+                  },
+                  {
+                    name: `twitter:creator`,
+                    content: data.site.siteMetadata.author.name,
+                  },
+                  {
+                    name: `twitter:title`,
+                    content: title,
+                  },
+                  {
+                    name: `twitter:description`,
+                    content: metaDescription,
+                  },
+                ].concat(
+                  keywords.length > 0 ? {
+                    name: 'keyboards',
+                    content: keywords.join(', ')
+                  } : []
+                )
+              }/>
+          )
+        }
+      }
+    />
   );
 };
 
