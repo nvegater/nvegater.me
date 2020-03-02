036f69593dc6393526f3c3b5d2e282a13426c70d
nvegater <nico_vt@protonmail.com>
2020-03-02 14:29:15 +0100

    created Bio Page

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -1,1 +1,53 @@
-export {}
+import React, {FC} from "react";
+import {StaticQuery, graphql} from 'gatsby'
+
+// TODO move query to graphQL file, configure top level schema. https://github.com/jimkyndemeyer/graphql-config-examples/tree/master/remote-schema-introspection
+type StaticQueryData = {
+  site: {
+    siteMetadata: {
+      description: string
+      social: {
+        github: string
+      }
+    }
+  }
+}
+// this query access siteMetadata in gatsby-config.js.
+const graphQLquery = graphql`
+  query {
+    site {
+      siteMetadata {
+        description
+        social {
+          github
+        }
+      }
+    }
+  }
+`;
+
+const Bio: FC = () => {
+  return (
+    <StaticQuery
+      query={graphQLquery}
+      render={
+        (data:StaticQueryData) => {
+        const {description, social} = data.site.siteMetadata;
+        return (
+        <div>
+          <h1>
+            {description}
+          </h1>
+          <p>
+            By Nicolas Vega
+            <br />
+            <a href={social.github}>Github</a>
+          </p>
+        </div>)
+      }}
+
+    />
+  );
+};
+
+export default Bio;
