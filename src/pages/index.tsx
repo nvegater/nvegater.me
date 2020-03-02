fbe091762a94df91a38d87331768ea62c9247e33
nvegater <nico_vt@protonmail.com>
2020-03-02 15:18:17 +0100

    added index. All the content is rendered inside of Layout, and included Head, Bio, and loop through all the posts.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,2 +1,95 @@
-// TODO add index page
-export {} // just to avoid TS errors when file is empty
+import React, {FC} from "react";
+import {graphql, Link} from "gatsby";
+
+import Layout from "../components/Layout";
+import Head from "../components/head";
+import Bio from "../components/bio";
+
+
+interface PageQueryData {
+  site: {
+    siteMetadata: {
+      title: string
+    }
+  }
+  allMarkdownRemark: {
+    edges: {
+      node: {
+        excerpt: string
+        fields: {
+          slug: string
+        }
+        frontmatter: {
+          date: string
+          title: string
+        }
+      }
+    }[]
+  }
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      filter: {frontmatter: {published: {ne: false}}}
+      sort: {fields: [frontmatter___date], order: DESC}
+    ) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`;
+
+interface Props {
+  readonly data: PageQueryData;
+}
+
+const Index:FC <Props> = ({data})=>{
+  const siteTitle = data.site.siteMetadata.title;
+  const posts = data.allMarkdownRemark.edges;
+  return (
+    <Layout title={siteTitle}>
+      <Head
+        title={"All posts"}
+        keywords={['blog', 'gatsby', 'markdown', 'react']}
+      />
+      <Bio/>
+      <article>
+        <div className={'page-content'}>
+          {
+            posts.map(({node}) => {
+                const title = node.frontmatter.title || node.fields.slug;
+                return (
+                  <div key={node.fields.slug}>
+                    <h3>
+                      <Link to={node.fields.slug}>{title}</Link>
+                    </h3>
+                    <small>{node.frontmatter.date}</small>
+                    <p dangerouslySetInnerHTML={{__html: node.excerpt}} />
+                  </div>
+                );
+              }
+            )
+          }
+        </div>
+      </article>
+    </Layout>
+  );
+};
+
+export default Index;
