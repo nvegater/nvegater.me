40d87f879a1d13aafaa0c639febe34dfbad84839
nvegater <nico_vt@protonmail.com>
2020-03-03 17:17:36 +0100

    added tag pages

--- src/templates/tag.tsx
+++ src/templates/tag.tsx
@@ -1,3 +1,97 @@
-// TODO add tag template
-export {} // just to avoid TS errors when file is empty
+import React, {FC} from "react";
+import {graphql, Link} from "gatsby";
+import Layout from "../components/Layout";
+import Head from "../components/head";
+interface PageQueryData {
+  site: {
+    siteMetadata: {
+      title: string
+    }
+  }
+  allMarkdownRemark: {
+    totalCount: number
+    edges: {
+      node: {
+        excerpt: string
+        fields: {
+          slug: string
+        }
+        frontmatter: {
+          date: string
+          title: string
+        }
+      }
+    }[]
+  }
+}
 
+export const pageQuery = graphql`
+  query TagPage($tag: String) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(limit: 1000, filter: {frontmatter: {tags: {in: [$tag]}}}) {
+      totalCount
+      edges {
+        node {
+          excerpt(pruneLength: 2500)
+          fields {
+            slug
+          }
+          frontmatter {
+            date
+            title
+          }
+        }
+      }
+    }
+  }
+`
+
+interface Props {
+  readonly data: PageQueryData
+  readonly pageContext: {
+    tag: string
+  }}
+
+const TagTemplate:FC<Props> = ({data,pageContext})=>{
+
+  const siteTitle = data.site.siteMetadata.title;
+  const {tag} = pageContext;
+  const posts = data.allMarkdownRemark.edges;
+
+  return(
+    <Layout title={siteTitle}>
+      <Head
+        title={`Posts tagged "${tag}"`}
+        keywords={[`blog`, `gatsby`, `javascript`, `react`, tag]}
+      />
+      <article>
+        <header>
+          <h1>Posts tagged {tag}</h1>
+        </header>
+        <div className={`page-content`}>
+          {
+            posts.map(({node}) => {
+                const title = node.frontmatter.title || node.fields.slug;
+                return (
+                  <div key={node.fields.slug}>
+                    <h3>
+                      <Link to={node.fields.slug}>{title}</Link>
+                    </h3>
+                    <small>{node.frontmatter.date}</small>
+                    <p dangerouslySetInnerHTML={{__html: node.excerpt}} />
+                  </div>
+                );
+              }
+            )
+          }
+        </div>
+      </article>
+    </Layout>
+  );
+};
+
+export default TagTemplate;
