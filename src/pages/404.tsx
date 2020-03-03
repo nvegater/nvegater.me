ff3932b224129ee401a5da50d7e5c842016dd781
nvegater <nico_vt@protonmail.com>
2020-03-03 16:16:57 +0100

    added not found page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,2 +1,42 @@
-// TODO add 404 page
-export {} // just to avoid TS errors when file is empty
+import React, {FC} from "react";
+import {graphql} from "gatsby";
+import Layout from "../components/Layout";
+import Head from "../components/head";
+interface PageQueryData {
+  site: {
+    siteMetadata: {
+      title: string
+    }
+  }
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
+
+interface Props {
+  readonly data: PageQueryData;
+}
+
+const NotFoundPage:FC<Props> = ({data})=>{
+
+  const siteTitle = data.site.siteMetadata.title;
+
+  return(
+    <Layout title={siteTitle}>
+      <Head title="404: Not found"/>
+      <h1>
+        not found...
+      </h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
