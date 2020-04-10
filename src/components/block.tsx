31098ee913da925a8f15583a1cd56d51c3ec105e
Nicolas Vega <nico_vt@protonmail.com>
2020-04-10 23:36:51 +0200

    added Layout and styling to the blocks in the chain. (TODO use https://github.com/pierpo/react-archer to add arrows between blocks)

--- src/components/block.tsx
+++ src/components/block.tsx
@@ -1,22 +1,35 @@
 import React, {FC} from 'react';
 import {Block as BlockType} from '../utils/blockchain'
+import styled from "styled-components";
+import {Box, Flex} from "rebass";
 
 
 interface BlockProps {
   block: BlockType;
 }
 
+const MorphFlex = styled(Flex)`
+  border-radius: 20px;
+  background: #feffff;
+  box-shadow:  20px 20px 60px #d8d9d9,
+               -20px -20px 60px #ffffff;
+`;
+
+const MorphBox = styled(Box)`
+  font-size: xx-small;
+`;
+
 const Block:FC<BlockProps> = ({block}) => {
 
 
   return (
-      <ul>
-        <li>Timestamp: {block.timestamp}</li>
-        <li>Index: {block.index}</li>
-        <li>Message: {block.content.message}</li>
-        <li>Hash: {block.hash}</li>
-        <li>Previous Hash: {block.previousHash}</li>
-      </ul>
+      <MorphFlex flexWrap="wrap">
+        <MorphBox width={1} p={1}>Hash: {block.hash.substr(0,10)}</MorphBox>
+        <MorphBox width={1} p={1}>Previous Hash: {block.previousHash.substr(0,10)}</MorphBox>
+        <MorphBox width={1} p={1}>Message: {block.content.message}</MorphBox>
+        <MorphBox width={1} p={1} >Timestamp: {block.timestamp}</MorphBox>
+        <MorphBox width={1} p={1}>Index: {block.index}</MorphBox>
+      </MorphFlex>
   )
 }
 
--- src/components/blockchaindisplay.tsx
+++ src/components/blockchaindisplay.tsx
@@ -1,6 +1,8 @@
 import React, {FC} from 'react';
 import { Block, BlockChain as BlockChainType} from '../utils/blockchain'
 import BlockDisplay from "./block";
+import {Box, Flex} from "rebass";
+
 
 
 interface BlockChainProps {
@@ -9,17 +11,38 @@ interface BlockChainProps {
 
 const BlockChainDisplay:FC<BlockChainProps> = ({blockChain}) => {
 
+  const widthForBlocks = (blockChain:BlockChainType):number => {
+    let numberOfBlocksPerLine = 7;
+    let lengthOfChain = blockChain.length;
+
+    let width:number;
+    if (lengthOfChain <= numberOfBlocksPerLine){
+      width = 1/numberOfBlocksPerLine;
+    } else {
+      width = 1/(lengthOfChain > numberOfBlocksPerLine ? numberOfBlocksPerLine :blockChain.length)
+    }
+    return width;
+  }
+
+
+
   return (
-    <>
-      {
-        blockChain.map((block:Block,index) => {
-          console.log(block)
-          return (
-          <BlockDisplay key={index} block={block}/>
-          )
-        })
-      }
-    </>
+      <Flex flexWrap="wrap" pt="20px" className="elements">
+        {
+          blockChain.map((block:Block,index) => {
+            console.log(block)
+            return (
+              <Box
+                width={widthForBlocks(blockChain)}
+                pr="5px"
+                minWidth={100}
+                minHeight={100}>
+                <BlockDisplay key={index} block={block}/>
+              </Box>
+            )
+          })
+        }
+      </Flex>
   )
 }
 
