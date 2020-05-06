771dcbb18708625b5528cd57ec00434ae5ea26b9
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-05-06 15:24:24 +0200

    added mdx introspected schema

--- src/pages/blockchain.tsx
+++ src/pages/blockchain.tsx
@@ -5,38 +5,38 @@ import BlockChainDisplay from "../components/blockchaindisplay";
 import {Block, BlockChain, lastBlockInChain, newBlock, startGenesisBlock} from '../utils/blockchain'
 
 
-const Blockchain:FC = () => {
-  const [blockChain,setBlockChain] = useState<BlockChain>([]);
-  const [lastBlock,setLastBlock] = useState<Block>();
-  const [lastBlockVisible,setLastBlockVisibility] = useState<boolean>(false);
+const Blockchain: FC = () => {
+  const [blockChain, setBlockChain] = useState<BlockChain>([]);
+  const [lastBlock, setLastBlock] = useState<Block>();
+  const [lastBlockVisible, setLastBlockVisibility] = useState<boolean>(false);
 
   const emptyBlockChain = blockChain.length === 0; // to show the blockchain
   const atLeastOneBlock = blockChain.length > 0; // to show the blockchain
   const morethanOneBlock = blockChain.length > 1; // to enable the get latest block button. The Blockchain.
 
   const handleBlockChainStart = () => {
-    let firstBlockChain:BlockChain = [];
-    let firstBlock:Block = startGenesisBlock()
-    firstBlockChain.push(firstBlock)
+    let firstBlockChain: BlockChain = [];
+    let firstBlock: Block = startGenesisBlock();
+    firstBlockChain.push(firstBlock);
     console.log(firstBlockChain);
     setBlockChain(firstBlockChain)
-  }
+  };
 
   const handleGetLatestBlock = () => {
     if (blockChain.length > 0) {
       setLastBlock(lastBlockInChain(blockChain));
       setLastBlockVisibility(true)
     }
-  }
+  };
 
   const handleHideLastBlock = () => {
     setLastBlockVisibility(false)
-  }
+  };
 
   const handleAddBlock = () => {
     const block = newBlock(blockChain);
-    setBlockChain([...blockChain,block]);
-  }
+    setBlockChain([...blockChain, block]);
+  };
 
   return (
     <Layout title="Account">
